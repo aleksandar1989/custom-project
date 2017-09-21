16450d9fbff49b56a95598e024a20210355cb013
Aleksandar Markovic <aleksandar.markoviccc@gmail.com>
2017-09-21 20:58:59 +0200

    better way for login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\User;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
 use Laravel\Socialite\Facades\Socialite;
@@ -43,6 +44,22 @@ class LoginController extends Controller
     }
 
     /**
+     * The user has been authenticated.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  mixed $user
+     * @return mixed
+     */
+    protected function authenticated(Request $request, $user)
+    {
+        if ($user->hasRole('admin')) {
+            return redirect('/admin');
+        }
+
+        return redirect('/');
+    }
+
+    /**
      * Redirect the user to the facebook authentication page.
      *
      * @return Response
@@ -63,7 +80,7 @@ class LoginController extends Controller
 
         $find_user = User::where('email', $social_user->email)->first();
 
-        if($find_user){
+        if ($find_user) {
             Auth::login($find_user);
         } else {
             $new_user = new User;
